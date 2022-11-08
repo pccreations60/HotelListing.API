0e4a1d83bdebf0db633c48779c45335cf2292afd
Paul Childs <pccreations.paul@gmail.com>
2022-11-08 03:26:30 -0500

    changes

---
+++ HotelListing.API/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HotelListing.API.Data;
+using Microsoft.Build.Framework.Profiler;
+
+namespace HotelListing.API.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Country, CreateCountryDto>().ReverseMap();
+        }
+    }
+}
---
+++ HotelListing.API/Models/Country/CreateCountyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelListing.API.Models.Country
+{
+    public class CreateCountyDto
+    {
+        public string Name { get; set; }
+        public string ShortName{ get; set; }
+    }
+}
